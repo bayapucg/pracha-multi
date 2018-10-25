21c84b51970d925a45b325d9ec207908682adcae
bayapucg <bayapu.edu004@gmail.com>
2018-10-25 15:10:33 +0530

    commit
    cahgnes

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -7,9 +7,16 @@
 							<li class="active"> 
 								<a href="index.php">Nav <span class="menu-arrow"></span></a>
 							</li>
-							<li class=""> 
-								<a href="slider.php">Sliders <span class="menu-arrow"></span></a>
+							<li class="submenu">
+								<a href="#" class="noti-dot"><span> Sliders </span> <span class="menu-arrow"></span></a>
+								<ul class="list-unstyled" style="display: none;">
+								
+									<li><a href="slider.php">Add Sliders</a></li>
+									<li><a href="slider-list.php">Sliders List</a></li>
+						
+								</ul>
 							</li>
+							
 							<li class=""> 
 								<a href="aboutus.php">About Us  <span class="menu-arrow"></span></a>
 							</li>
@@ -28,43 +35,8 @@
 							<li class=""> 
 								<a href="../preview.php">Preview<span class="menu-arrow"></span></a>
 							</li>
-							<!--<li class="submenu">
-								<a href="#" class="noti-dot"><span> Employees </span> <span class="menu-arrow"></span></a>
-								<ul class="list-unstyled" style="display: none;">
-									<li><a href="employees-list.php">All Employees</a></li>
-									<li><a href="addemployee.php">Add Employee</a></li>
-									<li><a href="holidays.php">Holidays</a></li>
-						
-								</ul>
-							</li>
-							<li class="submenu">
-								<a href="#" ><span> Payroll Management</span> <span class="menu-arrow"></span></a>
-								<ul class="list-unstyled" style="display: none;">
-									<li><a href="salarylist.php"> Employee Salary </a></li>
-									<li><a href="addsalery.php"> Add Salary </a></li>
-									<li><a href="payslip.php"> Genarate Payslip </a></li>
-								</ul>
-							</li>
-							<li class="submenu">
-								<a href="#" ><span>Employee Management</span> <span class="menu-arrow"></span></a>
-								<ul class="list-unstyled" style="display: none;">
-									<li><a href="attendence.php">Employee attendance </a></li>
-									<li><a href="shift-management.php">Shift Mangement </a></li>
-									<li><a class="active" href="leaves.php"><span>Leave Requests</span> <span class="badge bg-primary pull-right">1</span></a></li>
-									<li><a href="leaves-list.php">Leaves List </a></li>		
-								</ul>
-							</li>
-							<li class="submenu">
-								<a href="#" ><span>Employee communication </span> <span class="menu-arrow"></span></a>
-								<ul class="list-unstyled" style="display: none;">
-									<li><a href="chat.php">Chat </a></li>
-										
-								</ul>
-							</li>
-							<li class="">
-								<a href="salestrack.php" ><span>Salesman Man Track </span> </a>
-								
-							</li>-->
+							
+							
 							
 						</ul>
 					</div>
